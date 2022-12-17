76f16f861138adc6bd07a4da166173838db0d061
Zane Duffield <duffieldzane@gmail.com>
2022-12-17 14:21:51 +1100

    update template module

--- src/day_xx/mod.rs
+++ src/day_xx/mod.rs
@@ -1,25 +1,34 @@
-pub type Input = ...;
+pub struct Input {
+
+}
 
 #[aoc_generator(dayxx)]
 pub fn input_generator(input: &str) -> Input {
+
 }
 
 #[aoc(dayxx, part1)]
-pub fn part_1(input: &Input) ->  {
+pub fn part_1(input: &Input) -> u32 {
+
 }
 
 #[aoc(dayxx, part2)]
-pub fn part_2(input: &Input) ->  {
+pub fn part_2(input: &Input) -> u32 {
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use indoc::indoc;
 
     #[test]
     fn test() {
-        let input = input_generator("");
-        assert_eq!(part_1(&input), );
-        assert_eq!(part_2(&input), );
+        let input = input_generator(indoc! {
+            "
+            "
+        });
+        assert_eq!(part_1(&input),);
+        assert_eq!(part_2(&input),);
     }
 }
