02ce20e4990f5eaf90ea6d4f9f4a74d83303df6f
Zane Duffield <duffieldzane@gmail.com>
2023-05-27 18:45:22 +1000

    optimise day 12

--- src/day_12/mod.rs
+++ src/day_12/mod.rs
@@ -1,85 +1,54 @@
-use std::{cmp::Reverse, collections::BinaryHeap};
+use std::{
+    fmt::{Debug, Write},
+    ops::{Shl, Shr},
+    simd::u32x8,
+};
 
 use itertools::Itertools;
-use rayon::prelude::*;
-use rustc_hash::FxHashMap;
+use num::Integer;
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Pos {
-    x: i32,
-    y: i32,
+    x: usize,
+    y: usize,
 }
 
 impl From<(usize, usize)> for Pos {
     fn from((x, y): (usize, usize)) -> Self {
-        Self {
-            x: x as i32,
-            y: y as i32,
-        }
+        Self { x, y }
     }
+}
+
+// u32x8 is the fastest by a slim margin on my machine, but this should work with any SIMD type.
+type Lane = u32x8;
+type LaneElm = u32;
+const LANE_WIDTH: usize = LaneElm::BITS as usize;
+
+#[derive(Default, Clone, Copy)]
+struct Steps {
+    left: Lane,
+    right: Lane,
+    up: Lane,
+    down: Lane,
 }
 
 pub struct Input {
     start: Pos,
+    alt_starts: Vec<Pos>,
     end: Pos,
-    grid: Vec<u8>,
-    width: i32,
-    height: i32,
+    grid: Vec<Steps>,
+    width: usize,
+    height: usize,
 }
 
-impl Input {
-    fn get_elev(&self, Pos { x, y }: Pos) -> Option<u8> {
-        if x < 0 || y < 0 || x >= self.width {
-            None
-        } else {
-            self.grid.get((y * self.width + x) as usize).copied()
-        }
-    }
+fn set_lane_bit(lane: &mut Lane, x: usize) {
+    let (quot, rem) = x.div_rem(&LANE_WIDTH);
+    lane.as_mut_array()[quot] |= 1 << rem;
+}
 
-    fn nbours(&self, pos: Pos) -> [Option<Pos>; 4] {
-        let mut out = [None; 4];
-        if let Some(elv) = self.get_elev(pos) {
-            let max_elv = elv + 1;
-            [(-1, 0), (1, 0), (0, -1), (0, 1)]
-                .iter()
-                .enumerate()
-                .for_each(|(i, (dx, dy))| {
-                    let nb = Pos {
-                        x: pos.x + dx,
-                        y: pos.y + dy,
-                    };
-                    match self.get_elev(nb) {
-                        Some(n_elv) if n_elv <= max_elv => out[i] = Some(nb),
-                        _ => {}
-                    }
-                })
-        }
-        out
-    }
-
-    fn heuristic(&self, Pos { x, y }: Pos) -> u32 {
-        x.abs_diff(self.end.x) + y.abs_diff(self.end.y)
-    }
-
-    fn astar_min_cost(&self, start: Pos) -> Option<u32> {
-        let mut min_costs = FxHashMap::default();
-        let mut min_heap = BinaryHeap::new();
-        min_heap.push((Reverse(self.heuristic(start)), start, 0));
-
-        loop {
-            let (_, next, cost) = min_heap.pop()?;
-            if next == self.end {
-                return Some(cost);
-            }
-            let n_cost = cost + 1;
-            for n_pos in self.nbours(next).into_iter().flatten() {
-                if n_cost < *min_costs.get(&n_pos).unwrap_or(&u32::MAX) {
-                    min_costs.insert(n_pos, n_cost);
-                    min_heap.push((Reverse(n_cost + self.heuristic(n_pos)), n_pos, n_cost));
-                }
-            }
-        }
-    }
+fn get_lane_bit(lane: Lane, x: usize) -> LaneElm {
+    let (quot, rem) = x.div_rem(&LANE_WIDTH);
+    lane.as_array()[quot] & (1 << rem)
 }
 
 #[aoc_generator(day12)]
@@ -92,13 +61,19 @@ pub fn input_generator(input: &str) -> Input {
         .expect("at least one line is required")
         .len();
 
-    let mut grid = vec![];
+    let mut height_grid = vec![];
+    let mut alt_starts = vec![];
     let mut height = 0;
     for (y, line) in input.lines().enumerate() {
         height += 1;
-        grid.extend(line.bytes().enumerate().map(|(x, b)| match b {
+        height_grid.extend(line.bytes().enumerate().map(|(x, b)| match b {
             b'S' => {
                 start = Some((x, y).into());
+                alt_starts.push((x, y).into());
+                b'a'
+            }
+            b'a' => {
+                alt_starts.push((x, y).into());
                 b'a'
             }
             b'E' => {
@@ -109,35 +84,157 @@ pub fn input_generator(input: &str) -> Input {
         }));
     }
 
+    let mut grid = vec![Steps::default(); height];
+    grid.iter_mut().enumerate().for_each(|(i, row)| {
+        height_grid
+            .iter()
+            .skip(i * width)
+            .take(width)
+            .tuple_windows()
+            .enumerate()
+            .for_each(|(x, (&a, &b))| {
+                if b <= a + 1 {
+                    set_lane_bit(&mut row.right, x);
+                }
+                if a <= b + 1 {
+                    set_lane_bit(&mut row.left, x + 1);
+                }
+            });
+    });
+
+    for (a, b) in (0..height).tuple_windows() {
+        (0..width)
+            .map(|x| (x, height_grid[a * width + x], height_grid[b * width + x]))
+            .for_each(|(x, ha, hb)| {
+                if hb <= ha + 1 {
+                    set_lane_bit(&mut grid[a].down, x);
+                }
+                if ha <= hb + 1 {
+                    set_lane_bit(&mut grid[b].up, x);
+                }
+            });
+    }
+
     Input {
         start: start.expect("no start found"),
+        alt_starts,
         end: end.expect("no end found"),
         grid,
-        width: width as i32,
+        width,
         height,
     }
 }
 
+impl Input {
+    fn write_grid(
+        &self,
+        f: &mut std::fmt::Formatter<'_>,
+        ch: char,
+        accessor: impl Fn(Steps) -> Lane,
+    ) -> std::fmt::Result {
+        f.write_char('\n')?;
+        for y in 0..self.height {
+            let elm = accessor(self.grid[y]);
+            for x in 0..self.width {
+                f.write_char(match get_lane_bit(elm, x) {
+                    0 => '.',
+                    _ => ch,
+                })?;
+            }
+            f.write_char('\n')?;
+        }
+        Ok(())
+    }
+}
+
+impl Debug for Input {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.write_grid(f, '>', |s| s.right)?;
+        self.write_grid(f, '<', |s| s.left)?;
+        self.write_grid(f, '^', |s| s.up)?;
+        self.write_grid(f, 'v', |s| s.down)?;
+        Ok(())
+    }
+}
+
+const ALL_BUT_LAST_LANE: Lane = {
+    let mut lanes = [LaneElm::MAX; Lane::LANES];
+    lanes[Lane::LANES - 1] = 0;
+    Lane::from_array(lanes)
+};
+const ALL_BUT_FIRST_LANE: Lane = {
+    let mut lanes = [LaneElm::MAX; Lane::LANES];
+    lanes[0] = 0;
+    Lane::from_array(lanes)
+};
+
+fn solve(mut reachable: Vec<Lane>, input: &Input) -> usize {
+    let shift = Lane::splat(LANE_WIDTH as LaneElm - 1);
+
+    let mut steps = 0;
+    let mut new_reachable = reachable.clone();
+    while 0 == get_lane_bit(reachable[input.end.y], input.end.x) {
+        steps += 1;
+
+        for i in 0..reachable.len() {
+            let new_right_reachable = {
+                let to_step = reachable[i] & input.grid[i].right;
+                let stepped = to_step.shl(Lane::splat(1));
+                let overflow = (to_step & ALL_BUT_LAST_LANE)
+                    .rotate_lanes_right::<1>()
+                    .shr(shift);
+
+                stepped | overflow
+            };
+            let new_left_reachable = {
+                let to_step = reachable[i] & input.grid[i].left;
+                let stepped = to_step.shr(Lane::splat(1));
+                let overflow = (to_step & ALL_BUT_FIRST_LANE)
+                    .rotate_lanes_left::<1>()
+                    .shl(shift);
+
+                stepped | overflow
+            };
+
+            let new_up_reachable = {
+                reachable.get(i + 1).copied().unwrap_or_default()
+                    & input.grid.get(i + 1).map(|s| s.up).unwrap_or_default()
+            };
+            let new_down_reachable = {
+                let index = i.wrapping_sub(1);
+                reachable.get(index).copied().unwrap_or_default()
+                    & input.grid.get(index).map(|s| s.down).unwrap_or_default()
+            };
+
+            new_reachable[i] =
+                new_right_reachable | new_left_reachable | new_up_reachable | new_down_reachable;
+        }
+
+        std::mem::swap(&mut reachable, &mut new_reachable);
+    }
+    steps
+}
+
+fn init_reachable(input: &Input, starts: &[Pos]) -> Vec<Lane> {
+    let mut reachable = vec![Lane::default(); input.grid.len()];
+    for start in starts {
+        set_lane_bit(&mut reachable[start.y], start.x);
+    }
+
+    reachable
+}
+
 #[aoc(day12, part1)]
-pub fn part_1(input: &Input) -> u32 {
-    input.astar_min_cost(input.start).expect("no path found")
+pub fn part_1(input: &Input) -> usize {
+    #[cfg(debug_assertions)]
+    dbg!(input);
+
+    solve(init_reachable(input, &[input.start]), input)
 }
 
 #[aoc(day12, part2)]
-pub fn part_2(input: &Input) -> u32 {
-    let starts = (0..input.height)
-        .flat_map(|y| {
-            (0..input.width)
-                .map(move |x| Pos { x, y })
-                .filter(|&p| input.get_elev(p) == Some(b'a'))
-        })
-        .collect_vec();
-
-    starts
-        .into_par_iter()
-        .flat_map(|p| input.astar_min_cost(p))
-        .min()
-        .expect("no paths found")
+pub fn part_2(input: &Input) -> usize {
+    solve(init_reachable(input, &input.alt_starts), input)
 }
 
 #[cfg(test)]
@@ -158,5 +255,12 @@ mod tests {
         });
         assert_eq!(part_1(&input), 31);
         assert_eq!(part_2(&input), 29);
+    }
+
+    #[test]
+    fn test_big() {
+        let input = input_generator(include_str!("../../input/2022/day12.txt"));
+        assert_eq!(part_1(&input), 456);
+        assert_eq!(part_2(&input), 454);
     }
 }
